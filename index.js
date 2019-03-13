da46be24b14543482bfab293a1580ad62a6bd554
Kalkidan Lemma <k.lemma14@gmail.com>
2019-03-13 17:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function johnLennonFacts(facts) {
   var i = 0;
   var newFacts = [];
  while (i < 4) {
-   newFacts[i].push(`${facts[i]}` + "!!!");
+   newFacts[i] = facts.push(`${facts[i]}` + "!!!");
    i++;}
    return newFacts;
 }
